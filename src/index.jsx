00ec2d3650ee3cf4055a98399fd80a7082fbf436
shcherbak <shcherbak.ssa@gmail.com>
2020-12-13 20:53:36 +0300

    init: init project

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './assets/favicon.ico';
+import './styles/main.scss';
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('app'));
