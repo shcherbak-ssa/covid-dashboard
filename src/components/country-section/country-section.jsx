b8cb96a0559e57f28ddaf1a4915ba61aedf9a3ac
shcherbak <shcherbak.ssa@gmail.com>
2020-12-15 17:30:56 +0300

    feat: create country-section component

--- src/components/country-section/country-section.jsx
+++ src/components/country-section/country-section.jsx
@@ -3,43 +3,94 @@ import './country-section.scss';
 
 import { COUNTRY_OPTIONS_MENU_TYPE } from '../../constants';
 import { textLabelDefaultState, updateTextLabel, getSearchData } from '../../tools';
+import Base from '../base';
 import Section from '../section';
 
+const DEFAULT_SECTION_TITLE = 'Global';
+
 export default function CountrySection(props) {
-  const {title = 'Global', currentTheme, openFullscreen, /* selectedCountry */} = props;
+  const {apiData, currentTheme, openFullscreen, selectedCountry} = props;
+  const [sectionTitle, setSectionTitle] = useState('');
   const [textLabel, setTextLabel] = useState(textLabelDefaultState);
-  const [searchData, setSearchData] = useState(getSearchData(textLabel));
-  // const [apiData, setApiData] = useState(props.apiData);
-  // console.log(apiData, searchData);
+  const [content, setContent] = useState({});
 
   useEffect(() => {
     updateTextLabel('parameter', '', textLabel, setTextLabel);
   }, []);
 
+  useEffect(() => {
+    const title = selectedCountry ? selectedCountry.countryName : DEFAULT_SECTION_TITLE;
+    setSectionTitle(title);
+
+    const searchData = getSearchData(textLabel);
+    updateContent(searchData);
+  }, [selectedCountry]);
+
   const sectionProps = {
     sectionType: 'country',
     headerProps: {
-      title,
+      title: sectionTitle,
       currentTheme,
       textLabel,
       optionsMenuType: COUNTRY_OPTIONS_MENU_TYPE,
       updateApiData: (key, label) => {
         const updatedTextLabel = updateTextLabel(key, label, textLabel, setTextLabel);
-        setSearchData(getSearchData(updatedTextLabel));
+        const searchData = getSearchData(updatedTextLabel);
+        updateContent(searchData);
       },
     },
     openFullscreen: () => {
       openFullscreen({
-        currentFullscreenTitle: title,
+        currentFullscreenTitle: sectionTitle,
         currentFullscreenContent: CountrySectionFullscreenContent(),
       });
     },
   };
+
+  function updateContent(searchData) {
+    const searchObject = selectedCountry || apiData.global;
+    setContent(searchObject[searchData.key]);
+  }
   
   return (
     <Section {...sectionProps}>
-      {/* your code */}
+      <CountrySectionContent content={content} />
     </Section>
+  );
+}
+
+function CountrySectionContent({content}) {
+  const casesProps = {
+    type: 'cases',
+    title: 'Cases',
+    number: content.cases,
+  };
+  const deathsProps = {
+    type: 'deaths',
+    title: 'Deaths',
+    number: content.deaths,
+  };
+  const recoveredProps = {
+    type: 'recovered',
+    title: 'Recovered',
+    number: content.recovered,
+  };
+
+  return (
+    <div className="country-section-content">
+      <CountrySectionContentItem {...casesProps} />
+      <CountrySectionContentItem {...deathsProps} />
+      <CountrySectionContentItem {...recoveredProps} />
+    </div>
+  );
+}
+
+function CountrySectionContentItem({type, title, number}) {
+  return (
+    <div className="country-section-content-item">
+      <div className="country-section-content-title">{title}</div>
+      <Base.NumberView type={type} number={number} />
+    </div>
   );
 }
 
