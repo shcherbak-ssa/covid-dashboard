9143152d99cb6d153d8096aa423f86015a1f3cff
angoulema <eugenia.labokha@yandex.by>
2020-12-16 19:39:04 +0300

    feat: add list of countries to page

--- src/components/countries-section/countries-section.jsx
+++ src/components/countries-section/countries-section.jsx
@@ -5,37 +5,85 @@ import { textLabelDefaultState, updateTextLabel, getSearchData } from '../../too
 import Section from '../section';
 
 export default function CountriesSection(props) {
-  const {currentTheme, openFullscreen, /* setSelectedCountry */} = props;
+  const {currentTheme, openFullscreen, /* setSelectedCountry */ } = props;
   const [textLabel, setTextLabel] = useState(textLabelDefaultState);
   const [searchData, setSearchData] = useState(getSearchData(textLabel));
-  // const [apiData, setApiData] = useState(props.apiData);
+  const [apiData, /* setApiData */] = useState(props.apiData);
   // console.log(apiData, searchData);
+  const content = {
+    apiData: apiData,
+    searchData: searchData
+  };
 
   const sectionProps = {
     sectionType: 'countries',
     headerProps: {
-      title: 'Cases by country',
+      title: 'Countries',
       currentTheme,
       textLabel,
       updateApiData: (key, label) => {
         const updatedTextLabel = updateTextLabel(key, label, textLabel, setTextLabel);
         setSearchData(getSearchData(updatedTextLabel));
-      },
+      }
     },
     openFullscreen: () => {
       openFullscreen({
-        currentFullscreenTitle: 'Cases by country',
+        currentFullscreenTitle: 'Countries',
         currentFullscreenContent: CountriesSectionFullscreenContent(),
       });
-    },
+    }
   };
-  
-  return (
+
+  return ( // 
+    //{/* your code */}
     <Section {...sectionProps}>
-      {/* your code */}
+      <CountriesSectionContent {...content} />
     </Section>
   );
 }
+
+// взято из раздела таблицы
+function CountriesSectionContent(content) {
+  const data = content.apiData;
+  const parametres = content.searchData;
+  console.log(data, parametres);
+  const myData = [];
+  data.forEach((datum) => {
+    const name = datum.countryName;
+    const flag = datum.countryFlag;
+    const key = datum[parametres.key];
+    const parameter = key[parametres.parameter];
+    const obj = {
+      name: name,
+      flag: flag,
+      parameter: parameter
+    };
+    myData.push(obj);
+  });
+
+  return (
+    <div className="countries-section-content">
+      {myData.sort((a, b) => a.parameter > b.parameter ? -1 : 1)
+        .map((item) => {
+          // console.log(item);
+          return CountriesSectionContentItem(item);
+        })}
+    </div>
+  ); 
+}
+
+const CountriesSectionContentItem = (item) => {
+  const inside = (
+    <div className="countries-section-content-item" key={item.parameter}>
+      <div className="countries-section-content-flag"><img src={item.flag} alt={item.name} /></div>
+      <div className="countries-section-content-name">{item.name}</div>
+      <div className="countries-section-content-parameter">{item.parameter}</div>
+    </div>
+  );
+  console.log(inside);
+  return inside;
+} 
+
 
 function CountriesSectionFullscreenContent() {
   return (
