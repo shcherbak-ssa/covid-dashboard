83b1237bc76a20442d15ae3af92c646757987977
shcherbak <shcherbak.ssa@gmail.com>
2020-12-23 17:27:39 +0300

    refactor: rename parameter of getSeatchData function

--- test/search-data.test.js
+++ test/search-data.test.js
@@ -4,58 +4,58 @@ const TYPE_TEST_FIELD = 'type';
 const PARAMETER_TEST_FIELD = 'parameter';
 const MEASUREMENT_TEST_FIELD = 'measurement';
 
-describe('Search data from text label', () => {
+describe('Search data from options', () => {
   it('Only type field', () => {
-    const testTextLabel = {
+    const testOptions = {
       type: TYPE_TEST_FIELD,
     };
     const expectedSearchData = {
-      key: testTextLabel.type
+      key: testOptions.type
     };
 
-    const searchData = getSearchData(testTextLabel);
+    const searchData = getSearchData(testOptions);
     expect(searchData).toMatchObject(expectedSearchData);
   });
 
   it('Type and parameter fields', () => {
-    const testTextLabel = {
+    const testOptions = {
       type: TYPE_TEST_FIELD,
       parameter: PARAMETER_TEST_FIELD,
     };
     const expectedSearchData = {
-      key: testTextLabel.type,
-      parameter: testTextLabel.parameter,
+      key: testOptions.type,
+      parameter: testOptions.parameter,
     };
 
-    const searchData = getSearchData(testTextLabel);
+    const searchData = getSearchData(testOptions);
     expect(searchData).toMatchObject(expectedSearchData);
   });
 
   it('Type and measurement fields', () => {
-    const testTextLabel = {
+    const testOptions = {
       type: TYPE_TEST_FIELD,
       measurement: MEASUREMENT_TEST_FIELD,
     };
     const expectedSearchData = {
-      key: testTextLabel.type + testTextLabel.measurement
+      key: testOptions.type + testOptions.measurement
     };
 
-    const searchData = getSearchData(testTextLabel);
+    const searchData = getSearchData(testOptions);
     expect(searchData).toMatchObject(expectedSearchData);
   });
 
   it('All fields', () => {
-    const testTextLabel = {
+    const testOptions = {
       type: TYPE_TEST_FIELD,
       parameter: PARAMETER_TEST_FIELD,
       measurement: MEASUREMENT_TEST_FIELD,
     };
     const expectedSearchData = {
-      key: testTextLabel.type + testTextLabel.measurement,
-      parameter: testTextLabel.parameter
+      key: testOptions.type + testOptions.measurement,
+      parameter: testOptions.parameter
     };
 
-    const searchData = getSearchData(testTextLabel);
+    const searchData = getSearchData(testOptions);
     expect(searchData).toMatchObject(expectedSearchData);
   });
 });
