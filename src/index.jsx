90d5a4aa60098943d027546406f51458382f8c18
shcherbak <shcherbak.ssa@gmail.com>
2020-12-23 16:02:43 +0300

    feat: add loader

--- src/index.jsx
+++ src/index.jsx
@@ -10,14 +10,24 @@ import './styles/keyboard.css';
 import { loadData } from './api';
 import App from './components/app';
 
+const LOADER_ELEMENT_ID = 'loader';
 const ROOT_ELEMENT_ID = 'app';
 
 init();
 
 async function init() {
   const apiData = await loadData();
+  renderApp(apiData);
+}
+
+function renderApp(apiData) {
   ReactDOM.render(
     <App apiData={apiData} />,
-    document.getElementById(ROOT_ELEMENT_ID)
+    document.getElementById(ROOT_ELEMENT_ID),
+    removeLoader
   );
+}
+
+function removeLoader() {
+  document.getElementById(LOADER_ELEMENT_ID).remove();
 }
